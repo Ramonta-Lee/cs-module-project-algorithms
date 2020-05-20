5307e7baf5f72d3acd1563fd935abcb6f708b2a6
unknown <viola4lfe@gmail.com>
2020-05-20 16:56:49 -0400

    initial commit

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -1,11 +1,25 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+
+iterate over array to see every elem
+if elem matches 0 move to the end
+shift everything over to fill moved zero
+return the altered list
 '''
 def moving_zeroes(arr):
     # Your code here
+    for elem in arr:
+        if elem == 0:
+            # remove zero from index and append a zero
+            # shift everything to the left 1 (happens when you remove elem)
+            # increment down the for loop (done by for loop)
+            arr.remove(elem)
+            arr.append(0)
+        
+    return arr
 
-    pass
+    
 
 
 if __name__ == '__main__':
