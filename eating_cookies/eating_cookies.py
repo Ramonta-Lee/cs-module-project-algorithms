85c9f9aaea85691cf8926cefbce68fbc4410793b
unknown <viola4lfe@gmail.com>
2020-05-21 12:06:07 -0400

    complete day 1

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,11 +1,25 @@
 '''
 Input: an integer
 Returns: an integer
+options: eat 1,2,3 at a time
+don't know number of cookies
+
 '''
 def eating_cookies(n):
     # Your code here
+    # n - 1 
+    # n - 2 
+    # n - 3 
 
-    pass
+    if n < 0:
+        return 0
+    
+    if n == 0: # return 1 because there is 1 way to not eat any cookies versus to not eat negative cookies
+        return 1
+
+    else:
+        return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
